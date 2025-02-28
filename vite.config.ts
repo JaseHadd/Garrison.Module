4c054c82d05d01733f69c8c4220e40ec1ea090f7
Jase Haddleton <jasehaddleton@outlook.com.au>
2025-03-01 03:17:03 +1100

    first attempt at full build on github

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,46 @@
 import { defineConfig } from "vite";
 import copy from "rollup-plugin-copy"
 
-export default defineConfig({
-    optimizeDeps: {
-        include: ['src/styles/*.scss']
-    },
-    css: {
-        modules: {
-            localsConvention: 'camelCaseOnly'
-        }
-    },
-    build:
-    {
-        outDir: "scripts",
-        sourcemap: true,
-        minify: "esbuild",
-        rollupOptions: {
-            input: "src/module/main.ts",
-            output: {
-                dir: "dist/scripts",
-                entryFileNames: "[name].js",
-                format: "es"
+export default defineConfig(env => {
+    const devMode = env.mode === 'dev';
+
+    return {
+        optimizeDeps: {
+            include: ['src/styles/*.scss']
+        },
+        css: {
+            modules: {
+                localsConvention: 'camelCaseOnly'
             }
         },
-        watch:
+        build:
         {
-            buildDelay: 1200
-        }
-    },
-    plugins: [
-        copy({
-            targets: [
-                { src: "src/module.json", dest: "dist" },
-                { src: "src/templates", dest: "dist" },
-                { src: "src/styles", dest: "dist" },
-                { src: "icons", dest: "dist" },
-                { src: "lang", dest: "dist" }
-            ],
-            hook: "writeBundle",
-            copyOnce: true,
-            overwrite: true
-        })
-    ]
+            outDir: "scripts",
+            minify: "esbuild",
+            sourcemap: devMode,
+            rollupOptions: {
+                input: "src/module/main.ts",
+                output: {
+                    dir: "dist/scripts",
+                    entryFileNames: "[name].js",
+                    format: "es"
+                }
+            },
+            watch: devMode ? { buildDelay: 500 } : undefined
+        },
+        plugins: [
+            copy({
+                targets: [
+                    { src: "src/module.json", dest: "dist" },
+                    { src: "src/templates", dest: "dist" },
+                    { src: "src/styles", dest: "dist" },
+                    { src: "icons", dest: "dist" },
+                    { src: "lang", dest: "dist" }
+                ],
+                hook: "writeBundle",
+                copyOnce: true,
+                overwrite: true
+            })
+        ]
+    }
 });
